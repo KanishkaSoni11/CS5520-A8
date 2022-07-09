c6e557b2c83409793fb71f636476b97114891e69
rajatbhagat <rajat.bhagat@gmail.com>
2022-07-08 21:39:25 -0500

    Initial commit

---
+++ app/src/main/java/com/cs5520/cs5520_a8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs5520.cs5520_a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
