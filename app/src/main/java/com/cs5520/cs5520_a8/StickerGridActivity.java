1ed3c6f58c1a5e24b7814ad9bd3a7f66d02fdb52
rajatbhagat <rajat.bhagat@gmail.com>
2022-07-09 23:22:48 -0500

    Added the sticker grid activity

---
+++ app/src/main/java/com/cs5520/cs5520_a8/StickerGridActivity.java
@@ -0,0 +1,18 @@
+package com.cs5520.cs5520_a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class StickerGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_grid);
+    }
+}
