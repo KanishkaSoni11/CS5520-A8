41e687bc8db80a460a0f42660dfd9be6536d4690
tanishsehgal <tanishsehgal1234@gmail.com>
2022-07-10 11:23:37 -0400

    Storing Username in Local Storage

--- app/src/main/java/com/cs5520/cs5520_a8/MainActivity.java
+++ app/src/main/java/com/cs5520/cs5520_a8/MainActivity.java
@@ -3,11 +3,15 @@ package com.cs5520.cs5520_a8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +19,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = (Button)findViewById(R.id.login_button);
+        EditText username = findViewById(R.id.username);
+
+        sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                myEdit.putString("username", username.getText().toString());
+                myEdit.commit();
+
                 Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(intent);
             }
         });
+
+
+
+
+
     }
 }
