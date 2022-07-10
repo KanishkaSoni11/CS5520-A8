7a58d5527a0959ec7b499b5938c1acc19bf6f361
rajatbhagat <rajat.bhagat@gmail.com>
2022-07-09 23:24:52 -0500

    Added some grid view population methods and classes

--- app/src/main/java/com/cs5520/cs5520_a8/StickerGridActivity.java
+++ app/src/main/java/com/cs5520/cs5520_a8/StickerGridActivity.java
@@ -10,9 +10,26 @@ import android.widget.Toast;
 
 public class StickerGridActivity extends AppCompatActivity {
 
+    private final Integer[] imageIdArray = {
+            R.drawable.angel, R.drawable.angry, R.drawable.angry,
+            R.drawable.bored, R.drawable.happy
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker_grid);
+
+        GridView stickerGridView = (GridView) findViewById(R.id.sticker_grid_view);
+        StickerGridViewAdapter adapter = new StickerGridViewAdapter(this, imageIdArray);
+        stickerGridView.setAdapter(adapter);
+
+        stickerGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id) {
+                Toast.makeText(getBaseContext(), "Grid Item " + (position + 1) + " Selected",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cs5520/cs5520_a8/StickerGridViewAdapter.java
@@ -0,0 +1,50 @@
+package com.cs5520.cs5520_a8;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class StickerGridViewAdapter extends BaseAdapter {
+
+    private final Context context;
+    private final Integer[] imageIds;
+
+    public StickerGridViewAdapter(Context context, Integer[] imageIds) {
+        this.context = context;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getCount() {
+        return this.imageIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(this.context);
+            imageView.setLayoutParams(new GridView.LayoutParams(130, 130));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(16, 16, 16, 16);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(this.imageIds[position]);
+        return imageView;
+    }
+}
