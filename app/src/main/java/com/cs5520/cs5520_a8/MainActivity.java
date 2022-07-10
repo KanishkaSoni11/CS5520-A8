3706dddb1ba176dfc75835228505f2be833a0853
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-07-10 00:25:50 -0500

    real-time db

--- app/src/main/java/com/cs5520/cs5520_a8/MainActivity.java
+++ app/src/main/java/com/cs5520/cs5520_a8/MainActivity.java
@@ -3,6 +3,9 @@ package com.cs5520.cs5520_a8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onChildAdded1(View view) {
+        RealTimeDbActivity stickerExchangeDetails = new RealTimeDbActivity();
+        stickerExchangeDetails.onChildAdded1(view);
     }
 }
