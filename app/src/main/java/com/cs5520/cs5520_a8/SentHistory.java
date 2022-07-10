fce50d431cbc808b7303d6a00a60a74f4bfc44cb
nishamishra12 <nishamishra093@gmail.com>
2022-07-10 13:28:17 -0400

    Sent Activity and recyler view layout

---
+++ app/src/main/java/com/cs5520/cs5520_a8/SentHistory.java
@@ -0,0 +1,20 @@
+package com.cs5520.cs5520_a8;
+
+public class SentHistory {
+
+    private int sticker;
+    private int count;
+
+    public SentHistory(int sticker, int count) {
+        this.sticker = sticker;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getSticker() {
+        return sticker;
+    }
+}
---
+++ app/src/main/java/com/cs5520/cs5520_a8/SentHistoryActivity.java
@@ -0,0 +1,11 @@
+package com.cs5520.cs5520_a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class SentHistoryActivity extends AppCompatActivity {
+
+    List<SentHistory> sentHistoryList;
+    
+}
