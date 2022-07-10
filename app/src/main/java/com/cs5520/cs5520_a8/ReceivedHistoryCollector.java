c923f4ad7b63a2e12d76e5eb31c81c43dc6e3313
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-07-10 11:11:36 -0500

    collector for history

---
+++ app/src/main/java/com/cs5520/cs5520_a8/ReceivedHistoryCollector.java
@@ -0,0 +1,25 @@
+package com.cs5520.cs5520_a8;
+
+public class ReceivedHistoryCollector {
+    public String senderId;
+    public String dateSent;
+    public String sticker;
+
+    public ReceivedHistoryCollector(String senderId, String dateSent, String sticker) {
+        this.senderId = senderId;
+        this.dateSent = dateSent;
+        this.sticker = sticker;
+    }
+
+    public String getSenderId() {
+        return this.senderId;
+    }
+
+    public String getDateSent() {
+        return this.dateSent;
+    }
+
+    public String getSticker() {
+        return this.sticker;
+    }
+}
